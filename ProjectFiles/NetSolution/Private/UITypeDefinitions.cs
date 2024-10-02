ee9a967bd0e744ae21706cd3ad56a4bbdf5b77b3
Baron-USA <engineering@baronusa.com>
2024-10-02 15:03:08 -0500

    Add files via upload

--- ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -10,18 +10,13 @@ public class MainWindow : FTOptix.UI.Window
 {
 }
 
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
-public class WAN : FTOptix.UI.Panel
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "a7f84ad70b1faba37955f2e249f13bc2")]
+public class Dash : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
-public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
-public class DateAndTimeWidget : FTOptix.UI.Screen
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "343e9a553a1648e3de95b91f6d877b43")]
+public class DeviceSettingsWidget : FTOptix.UI.Screen
 {
 }
 
@@ -40,13 +35,8 @@ public class NetworkSettingsWidget : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "323ecbd80fe0dccc66307959b261f54a")]
-public class EmptyPanel : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "a7f84ad70b1faba37955f2e249f13bc2")]
-public class Dash : FTOptix.UI.Screen
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "acc9a2306cba9cdf618ea080ea59996c")]
+public class System_Information : FTOptix.UI.Screen
 {
 }
 
@@ -60,33 +50,8 @@ public class Data_Logging : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "acc9a2306cba9cdf618ea080ea59996c")]
-public class System_Information : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
-public class RequestAssistancePanel : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "abf458903710ef4d21ae0b2522aba4ff")]
-public class Vacuum : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "5c8b46c83f81724300956b1b05f17465")]
-public class Oil_Temp : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "ff7fc7484ad87068fc54662acbf26202")]
-public class Moisture : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "343e9a553a1648e3de95b91f6d877b43")]
-public class DeviceSettingsWidget : FTOptix.UI.Screen
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
 {
 }
 
@@ -102,6 +67,11 @@ public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
 
 [MapType(NamespaceUri = "HMI_6040_RMS", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
 public class LAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
+public class WAN : FTOptix.UI.Panel
 {
 }
 
@@ -122,5 +92,20 @@ public class Diagnostics : FTOptix.UI.Panel
 
 [MapType(NamespaceUri = "HMI_6040_RMS", Guid = "e348b14dbc7035688714c7ced8a72128")]
 public class ConnectionPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
+public class RequestAssistancePanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
+public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "HMI_6040_RMS", Guid = "323ecbd80fe0dccc66307959b261f54a")]
+public class EmptyPanel : FTOptix.UI.Panel
 {
 }
